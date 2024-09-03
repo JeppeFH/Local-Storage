0be6761cc387544bf116aa08cc0878565dfd9620
JeppeFH <jeppefinch@hotmail.dk>
2024-09-03 09:05:23 +0200

    update all

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,11 @@
-/*Navigation*/
-let burgerMenuOpen = document.querySelector(".burgerMenuOpen");
-let burgerMenuClose = document.querySelector(".burgerMenuClose");
-let navLinks = document.querySelector(".nav-links");
-/*Let = En literal der bruges til at deklarere en variabel.*/
-/*document = kobler det til ens html */
-/*querySelector =  Søger efter elementer i dokumentet, 
-der har class eller id*/
+import { productsList } from "./products_list.js";
+import { productsPage } from "./products_page.js";
 
-burgerMenuOpen.addEventListener("click", () => {
-  /*Fortæller at der skal være en Click event listener på array'en*/
-  navLinks.classList.add("active");
-  burgerMenuOpen.classList.add("active");
-  burgerMenuClose.classList.add("active");
-  /*Laver en classList på arrays for at manipulere class samtidig 
-  en add("active") på for så at kunne manipulere class i css. */
-});
+const app = {};
 
-burgerMenuClose.addEventListener("click", () => {
-  navLinks.classList.remove("active");
-  burgerMenuOpen.classList.remove("active");
-  burgerMenuClose.classList.remove("active");
-  /*Laver et click event på burgermenuclose som remover active class ved 
-  click. */
-});
+app.init = async () => {
+  productsList();
+  productsPage();
+};
+
+app.init();
